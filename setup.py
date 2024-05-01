122a54aeb6376ac7c762ed630cb13508cc7df74c
Nils-Markus Meister <meister.markus.MM@gmx.net>
2024-05-01 18:38:26 +0100

    setup.py fixed

--- setup.py
+++ setup.py
@@ -1,49 +1,19 @@
-import os
-import torch
-from setuptools import setup, find_packages
-from torch.utils.cpp_extension import CUDAExtension, BuildExtension, Extension
-
-# Retrieve OpenCV include and library directories from environment variables
-opencv_include = os.getenv('OPENCV_PATH', '/usr/local/include/opencv4')
-opencv_lib = os.getenv('OPENCV_LIB', '/usr/local/lib')
-
-# CUDA specific configuration
-extra_compile_args = {'cxx': ['-O2']}
-define_macros = []
-
-# Check if CUDA is available
-if torch.cuda.is_available():
-    define_macros += [('WITH_CUDA', None)]
-    extra_compile_args['nvcc'] = ['-O2']
-    extensions = [
-        CUDAExtension(
-            name='compute_clahe',
-            sources=['compute_clahe.cpp'],
-            include_dirs=[opencv_include],
-            library_dirs=[opencv_lib],
-            libraries=['opencv_core', 'opencv_imgproc'],
-            define_macros=define_macros,
-            extra_compile_args=extra_compile_args,
-        )
-    ]
-else:
-    extensions = [
-        Extension(
-            name='compute_clahe',
-            sources=['compute_clahe.cpp'],
-            include_dirs=[opencv_include],
-            library_dirs=[opencv_lib],
-            libraries=['opencv_core', 'opencv_imgproc'],
-            define_macros=define_macros,
-            extra_compile_args=extra_compile_args,
-        )
-    ]
+from setuptools import setup, Extension
+from torch.utils import cpp_extension
 
 setup(
-    name='clahe_extension',
-    version='0.1',
-    author='Your Name',
-    packages=find_packages(),
-    ext_modules=extensions,
-    cmdclass={'build_ext': BuildExtension},
+    name='clahe3d',
+    ext_modules=[
+        cpp_extension.CppExtension('torch_clahe', ['torch_clahe.cpp'])
+    ],
+    cmdclass={'build_ext': cpp_extension.BuildExtension},
+    version='0.98.0',
+    requires=["torch"]
+)
+
+Extension(
+    name='clahe3d',
+    sources=['torch_clahe.cpp'],
+    include_dirs=cpp_extension.include_paths(),
+    language='c++'
 )
