c496b19bbfd64e864ddac74f69f45bfb1062bd25
Nils-Markus Meister <meister.markus.MM@gmx.net>
2025-04-28 16:13:38 +0200

    fixed setup + requirements

--- setup.py
+++ setup.py
@@ -4,16 +4,20 @@ from torch.utils import cpp_extension
 setup(
     name='clahe3d',
     ext_modules=[
-        cpp_extension.CppExtension('torch_clahe', ['torch_clahe.cpp'])
+        cpp_extension.CppExtension(
+            name='torch_clahe',
+            sources=['torch_clahe.cpp'],
+            extra_compile_args={'cxx': ['-std=c++17']}
+        )
     ],
     cmdclass={'build_ext': cpp_extension.BuildExtension},
-    version='0.99.15',
+    version='0.99.19',
     requires=["torch"]
 )
 
-Extension(
-    name='clahe3d',
-    sources=['torch_clahe.cpp'],
-    include_dirs=cpp_extension.include_paths(),
-    language='c++'
-)
+# Extension(
+#     name='clahe3d',
+#     sources=['torch_clahe.cpp'],
+#     include_dirs=cpp_extension.include_paths(),
+#     language='c++'
+# )
